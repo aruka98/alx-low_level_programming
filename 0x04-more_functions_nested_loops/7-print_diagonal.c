7d95c4163420e2ef3af482b2d296eec477ed554c
Imane <73458139+aruka98@users.noreply.github.com>
2023-04-28 16:18:39 -0700

    seven

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,4 +7,17 @@
 
 void print_diagonal(int n)
 {
+	int i, j;
 
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
