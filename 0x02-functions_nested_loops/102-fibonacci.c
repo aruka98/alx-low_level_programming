708fc7043e6bb2a9b958b958baefb537bf2ab624
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-01 15:39:41 -0700

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+  * main - function
+  * Return: always 0
+*/
+
+int main(void)
+{
+	long int n_2, n_1, n, n1;
+	int i;
+
+	n_2 = 1;
+	n_1 = 2;
+
+	printf("%ld, %ld", n_2, n_1);
+
+	for (i = 0; i <= 24; i++)
+	{
+		n = n_1 + n_2;
+
+		printf(", %ld", n);
+
+		n1 = n + n_1;
+
+		printf(", %ld", n1);
+
+		n_1 = n1;
+		n_2 = n;
+	}
+	return (0);
+}
