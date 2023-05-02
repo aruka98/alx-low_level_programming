fd6c76e5951df6a895e7a3fc91213159fcb19fd9
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-02 15:21:55 -0700

    0 to 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  * print_rev - function reverses a string
+  * @s: char input
+*/
+
+void print_rev(char *s)
+{
+	int len, i;
+
+	len = _strlen(s);
+
+	for (i = len - 1; i >= 0; i--)
+		_putchar(*(s + i));
+	_putchar('\n');
+}
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
