c2042739e98e8abad70082eb2749f6c6e19e7c2e
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-04 16:48:06 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 /**
-* _strncat - function that concatenates first input with n character of the second input
-* @des: first input and return
+* _strncat - function that concatenates 2 inputs
+* @dest: first input and return
 * @src: second input
 * @n: number of caracters
 * Return: output char pointer
 */
 
-char *_strncat( char * dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 	int l, l_2, i;
 
@@ -21,10 +21,12 @@ char *_strncat( char * dest, char *src, int n)
 	else if (n > l_2)
 		k = l_2;
 
-	if ( n > 0)
+	if (n > 0)
 	{
 		for (i = 0; i < k; i++)
-				dest[l + k] = src[k];
-		dest[l + K] = '\0';
+		{
+			dest[l + i] = src[i];
+		}
+		dest[l + k] = '\0';
 	}
 }
