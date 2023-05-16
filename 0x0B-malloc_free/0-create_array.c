afd711b647021fea7b6af6376f60929829438c1e
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-16 13:55:29 -0700

    error malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,14 +14,14 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
-	if (size == 0)
+	ptr = malloc(size * sizeof(char));
+
+	if (size == 0 || ptr == NULL)
 	{
-		ptr = NULL;
 		return (ptr);
 	}
 	else
 	{
-		ptr = malloc(size * sizeof(char));
 		for (i = 0; i < size; i++)
 			ptr[i] = c;
 
