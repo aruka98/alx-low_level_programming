c7aec337f371504a33514b326b26119d63a81b63
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-22 15:50:25 -0700

    tasks 0 to 2

---
+++ 0x0E-structures_typedefls/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  * struct dog - structure
+  * @name: the name of the dog
+  * @age: the age of the dog
+  * @owner: the owner of the dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+#endif
