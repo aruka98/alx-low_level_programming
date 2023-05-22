35cb7ca4c80433959407b8fee7380481f16ee348
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-22 15:09:26 -0700

    all tasks

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,7 @@
+#ifndef _3FUNCTION_LIKE_MACRO_H
+#define _3FUNCTION_LIKE_MACRO_H
+
+#define ABS(x) if (x < 0)\
+	x = -x;
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef _4SUM_H
+#define _5SUM_H
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
