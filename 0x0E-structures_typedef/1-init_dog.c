942506eefcedf923f3c96660d9703d6f6aedccac
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-22 16:45:20 -0700

    task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+* init_dog - function of initialisation
+* @d: input struct dog
+* @name: input name of the dog
+* @age: dog
+* @owner: dog
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
