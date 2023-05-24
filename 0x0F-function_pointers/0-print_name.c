f76f37f413353b0d34f080bdedd7da6cc09197d4
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-24 13:00:20 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * print_name - function print name
+  * @name: input name
+  * @f: function pointer to print name
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
