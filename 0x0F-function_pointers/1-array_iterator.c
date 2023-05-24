85662182c3da10d96e35262bdf903f79035053d8
Imane <73458139+aruka98@users.noreply.github.com>
2023-05-24 13:37:03 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+  * array_iterator - function make actions in a array
+  * @array: input array pointer
+  * @size: of the array
+  * @action: function pointer
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
