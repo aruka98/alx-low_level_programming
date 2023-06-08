d81b4fa447c1ea0c39981697f9a971de137ac3d8
Imane <73458139+aruka98@users.noreply.github.com>
2023-06-08 14:21:26 -0700

    add 0 & 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * print_binary - fuct printd binary of a number
+  * @n: input long int
+*/
+
+void print_binary(unsigned long int n)
+{
+	int ciel, prt;
+
+	ciel = sizeof(n) * 8;
+	prt = 0;
+
+	while (ciel)
+	{
+		if (ciel & 1L << --ciel)
+		{
+			_putchar('1');
+			prt++;
+		}
+		else if (prt)
+			_putchar('0');
+	}
+	if (!prt)
+		_putchar('0');
+}
