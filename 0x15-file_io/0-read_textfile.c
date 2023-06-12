28d3d9e6c8662af8f06e712b14dabe274e552a45
Imane <73458139+aruka98@users.noreply.github.com>
2023-06-12 14:48:21 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t size;
 	char std[BUFF_SIZE * 8];
 
-	if (!filename || letters)
+	if (!filename || !letters)
 		return (0);
 	fd = open(filename, O_RDONLY);
 
