763dfc73373d853724fae390e14b02c593fcd409
Imane <73458139+aruka98@users.noreply.github.com>
2023-06-12 14:54:04 -0700

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * create_file - fct creates a file
+  * @filename: input file
+  * @text_content: input
+  * Return: output int
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, size;
+
+	size = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[size];)
+			size++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, size);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
